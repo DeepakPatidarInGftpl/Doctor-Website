eb1bdef96e01bd21201d1241ad3b76a714e0f650
05rahulkumar <rahulk160797@gmail.com>
2023-08-16 12:54:30 +0530

    purchase module url design & address of selected supplier change address also

--- dist/template/assets/plugins/fileupload/fileuploadcustom.js
+++
@@ -1,8 +0,0 @@
-$(document).ready(function($) {
-    if($('.custom-file-container').length > 0) {
-            //First upload
-            var firstUpload = new FileUploadWithPreview('myFirstImage')
-            //Second upload
-            var secondUpload = new FileUploadWithPreview('mySecondImage')
-        }
-  });
