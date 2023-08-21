632ba639ebe92673774cc5ed366ac962c1fe1c28
05rahulkumar <rahulk160797@gmail.com>
2023-08-21 16:56:01 +0530

    subcategory group,add,update view with route permission,when edit purchase modue then display additional discount

--- src/app/core-component/product/add-subcategory-group/add-subcategory-group-routing.module.ts
+++ src/app/core-component/product/add-subcategory-group/add-subcategory-group-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { AddSubcategoryGroupComponent } from './add-subcategory-group.component';
+import { RolesGuardGuard } from 'src/app/guards/roles-guard.guard';
 
 const routes: Routes = [
-  {path:'',component:AddSubcategoryGroupComponent}
+  {path:'',component:AddSubcategoryGroupComponent,canActivate: [RolesGuardGuard], 
+  data: { allowedRoles: ['add_subcategorygroup'] } }
 ];
 
 @NgModule({
--- src/app/core-component/product/edit-subcategory-group/edit-subcategory-group-routing.module.ts
+++ src/app/core-component/product/edit-subcategory-group/edit-subcategory-group-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { EditSubcategoryGroupComponent } from './edit-subcategory-group.component';
+import { RolesGuardGuard } from 'src/app/guards/roles-guard.guard';
 
 const routes: Routes = [
-  {path:'',component:EditSubcategoryGroupComponent}
+  {path:'',component:EditSubcategoryGroupComponent,canActivate: [RolesGuardGuard], 
+  data: { allowedRoles: ['change_subcategorygroup', ] } }
 ];
 
 @NgModule({
