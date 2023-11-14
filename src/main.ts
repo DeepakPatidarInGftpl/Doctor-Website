884094ae231c1c122e8749b5a9c02f13e83ac0f4
rahul-gftpl <rahul@gftpl.in>
2023-11-14 16:16:26 +0530

    jhj

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,23 @@
+// import { enableProdMode } from '@angular/core';
+// import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// import { AppModule } from './app/app.module';
+// import { environment } from './environments/environment';
+
+// if (environment.production) {
+//   enableProdMode();
+// }
+
+// platformBrowserDynamic().bootstrapModule(AppModule)
+//   .catch(err => console.error(err));
+
+
+
 import { enableProdMode } from '@angular/core';
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
+
+// Set the max listeners limit
+require('events').EventEmitter.defaultMaxListeners = 20; // Adjust the number as needed
 
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
